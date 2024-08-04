1d188feda2d4ac7e629e3b33c63091d0f4cbf6ee
mXrky4 <ryanjoelraymond@gmail.com>
2024-08-05 00:37:00 +0800

    Adding new_transaction method and flesh it out

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,25 @@
+import hashlib
+import json
+from time import time
+
+
+
+# This class responsible managing the chain
 class Blockchain(object):
     def __init__(self):
         self.chain =[]
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=100):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
     
     def new_transactions(self):
         # Adds a new transaction to the list of transactions
@@ -20,3 +34,20 @@ class Blockchain(object):
     def last_block(self):
         # Return the last Block in the chain
         pass
+
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+          
+        self.current_transaction.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount,
+        })
+        return self.last_block['index']+1
